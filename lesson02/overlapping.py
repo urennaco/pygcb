f68bc3c42a75ff254996c67e39d3295d457deb6c
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2024-01-16 08:50:18 -0500

    Create overlapping.py for first Python review

---
+++ lesson02/overlapping.py
@@ -0,0 +1,11 @@
+numbers1 = [10, 20, 17, 12, 13, 6, 7, 18, 9, 15]
+numbers2 = [15, 9, 17, 6, 16, 8, 19, 11, 19, 16]
+
+overlapping = []
+
+# your code here
+
+
+
+print('The overlapping numbers are: ')
+print(overlapping)
